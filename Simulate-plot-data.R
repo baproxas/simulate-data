81a570bf7b8e9c40b2d35cc04791b7c482455f05
Bryan Roxas <baproxas@email.arizona.edu>
2022-03-15 10:36:32 -0700

    test

--- Simulate-plot-data.R
+++ Simulate-plot-data.R
@@ -4,7 +4,8 @@
 # 2022-03-15
 
 # Simulate predictor variable
-
+x <- rnorm(n = 100)
 # Calculate response variable and add noise
-
+y <- 2 * x + rnorm(n = 100, sd = 0.2)
 # Plot table
+plot(x = x, y = y)
